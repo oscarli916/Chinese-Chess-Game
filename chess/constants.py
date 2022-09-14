1052a70223b347a94650ff1861d116d50c465f92
Oscar <oscar_li916@yahoo.com.hk>
2022-09-14 18:27:55 +0800

    chore: pack into exe

--- chess/constants.py
+++ chess/constants.py
@@ -1,5 +1,7 @@
 
 # 3rd-party libraries
+import os
+import sys
 import pygame
 
 
@@ -15,19 +17,31 @@ BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW_ORANGE = (252, 174, 63)
 
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
 # Pieces
-RED_KING = pygame.image.load("assets/r-k.png")
-BLACK_KING = pygame.image.load("assets/b-k.png")
-RED_ROOK = pygame.image.load("assets/r-r.png")
-BLACK_ROOK = pygame.image.load("assets/b-r.png")
-RED_HORSE = pygame.image.load("assets/r-h.png")
-BLACK_HORSE = pygame.image.load("assets/b-h.png")
-RED_CANNON = pygame.image.load("assets/r-c.png")
-BLACK_CANNON = pygame.image.load("assets/b-c.png")
-RED_GUARD = pygame.image.load("assets/r-g.png")
-BLACK_GUARD = pygame.image.load("assets/b-g.png")
-RED_ELEPHANT = pygame.image.load("assets/r-e.png")
-BLACK_ELEPHANT = pygame.image.load("assets/b-e.png")
-RED_SOLDIER = pygame.image.load("assets/r-s.png")
-BLACK_SOLDIER = pygame.image.load("assets/b-s.png")
-EMPTY = pygame.image.load("assets/empty.png")
+RED_KING = pygame.image.load(resource_path(r"assets\r-k.png"))
+BLACK_KING = pygame.image.load(resource_path(r"assets\b-k.png"))
+RED_ROOK = pygame.image.load(resource_path(r"assets\r-r.png"))
+BLACK_ROOK = pygame.image.load(resource_path(r"assets\b-r.png"))
+RED_HORSE = pygame.image.load(resource_path(r"assets\r-h.png"))
+BLACK_HORSE = pygame.image.load(resource_path(r"assets\b-h.png"))
+RED_CANNON = pygame.image.load(resource_path(r"assets\r-c.png"))
+BLACK_CANNON = pygame.image.load(resource_path(r"assets\b-c.png"))
+RED_GUARD = pygame.image.load(resource_path(r"assets\r-g.png"))
+BLACK_GUARD = pygame.image.load(resource_path(r"assets\b-g.png"))
+RED_ELEPHANT = pygame.image.load(resource_path(r"assets\r-e.png"))
+BLACK_ELEPHANT = pygame.image.load(resource_path(r"assets\b-e.png"))
+RED_SOLDIER = pygame.image.load(resource_path(r"assets\r-s.png"))
+BLACK_SOLDIER = pygame.image.load(resource_path(r"assets\b-s.png"))
+EMPTY = pygame.image.load(resource_path(r"assets\empty.png"))
